57b653af8d6bca2b17c64b6e98dfb2af3114971c
diegogrebate <diego.grebate@gmail.com>
2025-01-25 19:28:12 +0100

    lint changes

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
   return (
     <>
       <Navbar />
-      <Hero ctaRef={ctaRef} />
+      <Hero ctaRef={ctaRef as React.RefObject<HTMLDivElement>} />
       <Features />
       <Location />
       <FAQs />
